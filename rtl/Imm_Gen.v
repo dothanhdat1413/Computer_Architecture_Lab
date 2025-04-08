b709cb24c42bc29973ef12d16c60da1955d430a1
dothanhdat1413 <datdo2525@gmail.com>
2025-04-08 12:54:46 +0700

    Update

--- rtl/Imm_Gen.v
+++ rtl/Imm_Gen.v
@@ -11,10 +11,10 @@ localparam U_FORMAT = 3'b100;
 
 always @(*) begin
     case (opcode)
-        I_S_FORMAT: imm_out = { {20{imm[11]}}, imm[11:0] }; // I-format & S-format instructions
-        B_FORMAT: imm_out = { {19{imm[11]}}, imm[11:0], 1'b0 };   // B-type instructions
-        U_FORMAT: imm_out = { imm, 12'b0 }; // U-type instructions
-        default: imm_out = 32'b0; // Default case to avoid latches
+        I_S_FORMAT: imm_out = { {20{imm[11]}}, imm[11:0] };
+        B_FORMAT: imm_out = { {19{imm[11]}}, imm[11:0], 1'b0 }; 
+        U_FORMAT: imm_out = { imm, 12'b0 }; 
+        default: imm_out = 32'b0; 
     endcase
 end
 
