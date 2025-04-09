61969d4b2143fe74ecdbe93525e4329bf2305641
datdo2525@gmail.com <datdo2525@gmail.com>
2025-04-09 20:08:44 +0700

    Testing

--- test/test_Imm_Gen.v
+++ test/test_Imm_Gen.v
@@ -1,3 +1,14 @@
 module test_Imm_Gen();
     
+    reg [2:0] opcode;
+    reg [20:0] imm;
+    wire [31:0] imm_out;
+
+    Imm_Gen DUT(
+        .opcode(opcode),
+        .imm(imm),
+        .imm_out(imm_out)
+    );
+
+
 endmodule
